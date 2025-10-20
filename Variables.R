ca3f746e7352662875ff7958424937869ffb5f2c
Jaffer Junaidi <jafferjunaidi@outlook.com>
2025-10-20 23:15:15 +0500

    variables

---
+++ Variables.R
@@ -0,0 +1,14 @@
+#Firstname<-'Jaffer'
+#Lastname<-'Junaidi'
+
+#Firstname<-'Kamran'
+#Lastname<-'Jitoi'
+#print(Firstname)
+#print(Lastname)
+
+'Abid'->.Firstname
+'Khan'->Last_name
+print(.Firstname)
+print(Last_name)
+print("Hello World")
+
