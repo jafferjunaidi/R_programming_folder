efdd383e42f03166a4ddbc37ed26762d8f0ff21e
Jaffer Junaidi <jafferjunaidi@outlook.com>
2025-10-20 23:08:56 +0500

    operators

---
+++ Operators.R
@@ -0,0 +1,38 @@
+#Relational & Arithmetic Operators
+num1 <- 10
+num2 <- 20
+
+print(num1+num2) # Addition
+print(num1-num2) # Subtraction
+print(num1*num2)# Multiplication
+print(num1/num2) # Division
+print(num1%%num2) # Modulus/remainder
+print(num1%/%num2) # Quotient
+
+#Vector Type
+num3 <- c(10,20,30)
+num4 <- c(40,50,60)
+
+print(num3+num4) # Addition
+print(num3-num4) # Subtraction
+print(num3*num4) # Multiplication
+print(num3/num4) # Division
+print(num3%%num4) # Modulus/remainder
+
+#Relational Operator
+num5 <- 10
+num6 <- 50
+
+print(num5>num6) # greater than
+print(num5>=num6) # greater than equals to
+print(num5<num6) # less than
+print(num5<=num6) # less than equals to
+print(num5==num6) # equals to strictly
+print(num5!=num6) # not equals to strictly
+
+# Logical Operators 
+print(TRUE&FALSE)
+print(TRUE&TRUE)
+print(FALSE|TRUE)
+print(FALSE||TRUE)
+print(TRUE&&FALSE)
